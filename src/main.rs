40911edde9002d9cda68dddf25b921504d3cd07d
Rich Neswold <neswold@fnal.gov>
2021-06-29 14:48:20 -0500

    add: Add `get` subcommand parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::clap_app;
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = clap_app!(
+        myapp =>
+            (version: "0.1.0")
+            (about: "Command line utility to access Fermilab's accelerator data")
+            (@subcommand get =>
+             (about: "get device data")
+             (@arg DRF: +required "specified the device and rate for acquisition")
+            )
+    ).get_matches();
 }
