3ee78a51806b1e263a8a752a316df011913ba94a
Rich Neswold <neswold@fnal.gov>
2021-06-30 15:48:45 -0500

    change: try to mirror arguments/subcommands in Beau's document.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
-use clap::clap_app;
+use clap::{clap_app, crate_version, crate_name};
 
 fn main() {
     let _matches = clap_app!(
-        myapp =>
-            (version: "0.1.0")
+        (crate_name!()) =>
+            (version: crate_version!())
             (about: "Command line utility to access Fermilab's accelerator data")
             (@subcommand get =>
-             (about: "get device data")
-             (@arg DRF: +required "specified the device and rate for acquisition")
+             (about: "Retrieves accelerator data")
+             (aliases: &["read", "request"])
+             (@subcommand history =>
+              (about: "Makes the retrieval get historical data from a logger")
+              (@arg START: -s --start <TIME> +required "sets the start time of the range")
+              (@arg END: -e --end <TIME> !required "sets the end time of the range")
+              (@arg DRF: +required "specifies the device and rate for acquisition")
+             )
+             (@subcommand live =>
+              (about: "Retrieves live data from the accelerator")
+              (@arg DRF: +required "specifies the device and rate for acquisition")
+             )
             )
-    ).get_matches();
+            (@subcommand put =>
+             (about: "Updates the value of a device")
+             (aliases: &["modify", "update"])
+             (@arg DRF: +required "specifies the device to be modified")
+             (@arg VALUE: +required "specifies the new value for the device")
+            )
+    )
+    .get_matches();
 }
